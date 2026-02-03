60f3cd7bcc8774ffb84a363ca93723035c8c94c5
illefeliz72 <elifelize.herreria@lorma.edu>
2026-02-03 12:30:25 +0800

    bruh

--- activity2/MethodArray.java
+++ activity2/MethodArray.java
@@ -1,3 +1,4 @@
+package activity2;
 
 import java.util.Scanner;
 
--- activity4/App.java
+++ activity4/App.java
@@ -3,21 +3,26 @@ package activity4;
 import java.util.Scanner;
 
 public class App {
-    static String[] subjects = { "COMPRO1", "COMPRO2", "OOP", "DSA", "MMW" };
-    static double[][] scores = new double[50][subjects.length];
-    static String[] studentNames = new String[50];
-    static int studentCount = 0;
+    static String[] subject = new String[10];
+    static double[] prelim = new double[10];
+    static double[] midterm = new double[10];
+    static double[] finals = new double[10];
     static Scanner sc = new Scanner(System.in);
+    static int count = 0;
+    static int choice = 0;
 
     public static void main(String[] args) {
 
-        while (true) {
+        try {
             System.out.println("""
                     \nMAIN MENU
                     [1] Add Grade for subject
                     [2] Exit
                     """);
+           System.out.print("Enter Choice: "); 
+           choice = sc.nextInt(); sc.nextLine();
 
+        } catch (Exception e) {
 
         }
 

91a4d811d79bd10b6637c0dd0151846c7d0c55c4
illefeliz72 <elifelize.herreria@lorma.edu>
2026-03-23 11:33:06 +0800

    meow

---
+++ practice/week11/server/src/main/java/com/server/Main.java
@@ -0,0 +1,9 @@
+package com.server;
+
+public class Main {
+    public static void main(String[] args) {
+        // Instantiate our new ChatServer and start the infinite listening loop.
+        ChatServer server = new ChatServer();
+        server.start();
+    }
+}

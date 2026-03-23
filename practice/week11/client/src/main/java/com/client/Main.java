10ec970015e054fcd55cd400ad1a8bdeaf14d28d
illefeliz72 <elifelize.herreria@lorma.edu>
2026-03-23 12:11:53 +0800

    clienserer

--- practice/week11/client/src/main/java/com/client/Main.java
+++
@@ -1,9 +0,0 @@
-package com.client;
-
-public class Main {
-    public static void main(String[] args) {
-        // Instantiate and start our ChatClient
-        ChatClient client = new ChatClient();
-        client.start();
-    }
-}
